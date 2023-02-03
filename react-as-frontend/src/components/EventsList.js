851788a654d6b19219ce2852e804c6284c659a5b
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-03 20:52:21 +0530

    Use prop-types for strict checking

--- react-as-frontend/src/components/EventsList.js
+++ react-as-frontend/src/components/EventsList.js
@@ -1,15 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 function EventsList({ events }) {
   return (
     <div>
       {events.map((event) => (
         <div>
           <h3>{event.title}</h3>
-          <p>{event.description.substring(0, 150)}...</p>
+          <p>{event.description.substring(0, 150)}</p>
           <p>{event.date}</p>
         </div>
       ))}
     </div>
   );
 }
+
+EventsList.propTypes = [
+  {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    date: PropTypes.string,
+  },
+];
 
 export default EventsList;
