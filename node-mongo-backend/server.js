99b2d3a31ad8a2f49a03e893243136439941dee8
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-07 20:49:47 +0530

    Fix full stack deployment to vercel issue

--- node-mongo-backend/server.js
+++ node-mongo-backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import cors from 'cors';
 
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -8,11 +9,11 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../react-as-frontend/build')));
+// app.use(express.static(path.join(__dirname, '../react-as-frontend/build')));
 
-app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../react-as-frontend/build/index.html'));
-});
+// app.get(/^(?!\/api).+/, (req, res) => {
+//   res.sendFile(path.join(__dirname, '../react-as-frontend/build/index.html'));
+// });
 
 const events = [
   {
@@ -42,11 +43,26 @@ const events = [
 ];
 
 app.use(morgan('tiny'));
+app.use(cors());
+
 app.use(express.json());
 
 app.get('/api/events', (req, res) => {
   res.send(events);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('/', (req, res) => {
+    app.use(
+      express.static(path.resolve(__dirname, '../react-as-frontend', 'build'))
+    );
+    res.sendFile(
+      path.resolve(__dirname, '../react-as-frontend', 'build', 'index.html')
+    );
+  });
+} else {
+  console.log('Not getting NODE_ENV!');
+}
 
 const PORT = process.env.PORT || 8000;
 
