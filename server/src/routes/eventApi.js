f8d581e5d8d501214295af0f6447b11cbceebe32
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-22 17:44:21 +0530

    Add wrapAsync to cleanup try catch block

--- server/src/routes/eventApi.js
+++ server/src/routes/eventApi.js
@@ -4,61 +4,55 @@ import express from 'express';
 import Event from '../models/event.js';
 import createEventController from '../controllers/createEventController.js';
 import createEventPersistence from '../injectables/events/createEventPersistence.js';
-import AppError from '../utils/AppError.js';
-
-const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
-
-if (admin.apps.length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.cert(credentials),
-  });
-}
+import wrapAsync from '../utils/wrapAsync.js';
 
 const router = express.Router();
 
-// Fetch All Events
 router.get('/api/events', async (req, res) => {
-  // Mongoose interaction
   const events = await Event.find({}).sort({ _id: -1 });
   res.send(events);
 });
 
-router.use(async (req, res, next) => {
-  const { authtoken } = req.headers;
+// const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
 
-  if (authtoken) {
-    try {
-      req.user = (await admin.auth().verifyIdToken(authtoken)) || {};
+// if (admin.apps.length === 0) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(credentials),
+//   });
+// }
 
-      console.log(req.user);
-    } catch (e) {
-      res.sendStatus(400);
-    }
+// router.use(async (req, res, next) => {
+//   const { authtoken } = req.headers;
 
-    if (req.user) {
-      next();
-    } else {
-      res.sendStatus(401);
-    }
-  }
-});
+//   if (authtoken) {
+//     try {
+//       req.user = (await admin.auth().verifyIdToken(authtoken)) || {};
 
-router.post('/api/events', async (req, res, next) => {
-  try {
+//       console.log(req.user);
+//     } catch (e) {
+//       res.sendStatus(400);
+//     }
+
+//     if (req.user) {
+//       next();
+//     } else {
+//       res.sendStatus(401);
+//     }
+//   }
+// });
+
+router.post(
+  '/api/events',
+  wrapAsync(async (req, res) => {
     const { title, description, date } = req.body;
-
-    console.log({ title, description, date });
-
     const newEvent = await createEventController(
       { createEventPersistence },
       { title, description, date }
     );
 
     res.send(newEvent);
-  } catch (err) {
-    next(err);
-  }
-});
+  })
+);
 
 router.use((err, req, res, next) => {
   const { status = 500, defaultMessage = 'Something went wrong!' } = err;
