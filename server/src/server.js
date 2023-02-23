75101f097ef4a2e1685da033eb3f9890e5c0aaa4
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-23 18:06:08 +0530

    Unit test setup and first test case to test fetch events

--- server/src/server.js
+++ server/src/server.js
@@ -19,3 +19,5 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+export default app;
---
+++ server/test/eventApi.js
@@ -0,0 +1,22 @@
+import { describe, it } from 'mocha';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../src/server.js';
+
+process.env.NODE_ENV = 'test';
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('Test GET route /api/tasks', () => {
+  it('It should return all tasks', (done) => {
+    chai
+      .request(server)
+      .get('/api/events')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        done();
+      });
+  });
+});
