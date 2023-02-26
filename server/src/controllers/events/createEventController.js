cc66dae2c983315722b9055b1cb80fd5f73b81a0
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-26 08:49:52 +0530

    Encapsulate event entity

--- server/src/controllers/events/createEventController.js
+++ server/src/controllers/events/createEventController.js
@@ -1,16 +1,23 @@
 import createEventPersistence from '../../interfaces/events/createEventPersistence.js';
 import wrapAsync from '../../utils/wrapAsync.js';
-import EventDTO from '../../dtos/events/EventDTO.js';
+import * as eventEntity from '../../entities/events/eventEntity.js';
 
 const createEvent =
   ({ createEventPersistence },
   wrapAsync(async (req, res) => {
     const { title, description, date } = req.body;
 
-    const eventDTO = new EventDTO({ title, description, date });
-    eventDTO.validate();
+    eventEntity.setDefaultEventEntity({
+      title,
+      description,
+      date,
+    });
+    eventEntity.defaultEventEntity();
+    eventEntity.validate();
 
-    const newEvent = await createEventPersistence(eventDTO);
+    const newEvent = await createEventPersistence(
+      eventEntity.defaultEventEntity()
+    );
     res.status(201).send(newEvent);
   }));
 
