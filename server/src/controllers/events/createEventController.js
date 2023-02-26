ace9abfd93ed9ec453f9ae462db14c5230eb5643
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-02-26 09:05:28 +0530

    Use suitable name for collection of event entity functions

--- server/src/controllers/events/createEventController.js
+++ server/src/controllers/events/createEventController.js
@@ -1,21 +1,21 @@
 import createEventPersistence from '../../interfaces/events/createEventPersistence.js';
 import wrapAsync from '../../utils/wrapAsync.js';
-import * as eventEntity from '../../entities/events/eventEntity.js';
+import * as eventObject from '../../entities/events/eventEntity.js';
 
 const createEvent =
   ({ createEventPersistence },
   wrapAsync(async (req, res) => {
     const { title, description, date } = req.body;
 
-    eventEntity.setDefaultEventEntity({
+    eventObject.setDefaultEventEntity({
       title,
       description,
       date,
     });
-    const eventData = eventEntity.defaultEventEntity();
-    eventEntity.validate();
+    const eventEntity = eventObject.defaultEventEntity();
+    eventObject.validate();
 
-    const newEvent = await createEventPersistence(eventData);
+    const newEvent = await createEventPersistence(eventEntity);
     res.status(201).send(newEvent);
   }));
 
