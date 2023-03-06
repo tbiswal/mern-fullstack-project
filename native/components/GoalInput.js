ceba4eae3deb01facb7ba98ecc904afde6e1cc4a
tanmayabiswal <tan.biswal@rapidfunnel.com>
2023-03-06 12:08:01 +0530

    Initial react native setup

---
+++ native/components/GoalInput.js
@@ -0,0 +1,43 @@
+import { StyleSheet, View, TextInput, Button } from "react-native";
+import { useState } from "react";
+
+GoalInput = (props) => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText('');
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your course goal!"
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      ></TextInput>
+      <Button title="Add Goal" onPress={addGoalHandler} />
+    </View>
+  );
+};
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
